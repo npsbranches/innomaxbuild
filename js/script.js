12eb31b6f27dc20680c73a05af874db0c5f3cd36
npsbranches <45974239+npsbranches@users.noreply.github.com>
2019-05-09 10:11:30 +0500

    1.6

--- js/script.js
+++ js/script.js
@@ -26,3 +26,24 @@ $(function() {
 
     changeBackground();        
 });
+
+if ($(".about-numbers").length) {
+    var show = true;
+    var countbox = ".about-numbers";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 600 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.counter-number').css('opacity', '1');
+            $('.counter-number').spincrement({
+                thousandSeparator: "",
+                duration: 4000
+            });
+            show = false;
+        }
+    });
+}
